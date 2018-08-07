f66c1a1d03a17b60cc51ad77eee6a891460935b3
Michael_Zuo <zuoshuai@moore.ren>
2018-08-07 16:43:52 +0800

    完善环形倒计时view

--- CycleCountDown/MZDrawView.swift
+++ CycleCountDown/MZDrawView.swift
@@ -7,50 +7,11 @@
 //
 
 import UIKit
-
+fileprivate let  Margin: CGFloat = 5.0
 class MZDrawView: UIView {
-    // MARK: property
-    var lineWidth: CGFloat = 5.0
-    var displayLink: CADisplayLink?
-    var backPathColor: UIColor = UIColor.clear
-    var cycleColor: UIColor?
-    // 倒计时时间
-    var duration: Double = 0.0 {
-        didSet {
-            if duration < 0.0 { return }
-            // 先停掉link
-            
-                invalidateLink()
-            
-           // 在开启link
-            self.displayLink = CADisplayLink(target: self, selector: #selector(countDown))
-            self.displayLink?.add(to: RunLoop.current, forMode: .commonModes)
-            
-        }
-    }
-    var progress: Double = 1.0 {
-        didSet {
-            if progress < 0 || progress > 1 {
-                return
-            }
-            self.setNeedsDisplay()
-        }
-    }
-
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        
-    }
-
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-
     // public func
-    open func drawCycleCountDown(center: CGPoint = CGPoint.zero, rad: CGFloat = 0.0) {
-        self.setNeedsDisplay()
-    }
-
+    /// 在不用的时候要call一下 停掉link
+    // 比如 vc的deinit方法里
     open func invalidateLink() {
         guard let displayLink = self.displayLink else {
             return
@@ -60,7 +21,6 @@ class MZDrawView: UIView {
     }
 
     // private func
-
     /// 倒计时 1/60s调用一次
     @objc private func countDown() {
         if progress < 0.0 || progress > 1.0 {
@@ -74,28 +34,85 @@ class MZDrawView: UIView {
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
     override func draw(_ rect: CGRect) {
-        let radius = (min(rect.size.width, rect.size.height) - lineWidth) * 0.5
-        let center = CGPoint(x: rect.size.width * 0.5, y: rect.size.height * 0.5)
-        let backPath = UIBezierPath()
+        // 没有自定义半径和中心的话取默认
+        let radius = self.radius == 0.0 ? (min(rect.size.width, rect.size.height) - lineWidth) * 0.5 : self.radius
+        let center = self.cycleCenter == CGPoint.zero ? CGPoint(x: rect.size.width * 0.5, y: rect.size.height * 0.5) : self.cycleCenter
+        
+        let backPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
         backPath.lineWidth = lineWidth
-        backPathColor.set()
         backPath.lineCapStyle = .round
         backPath.lineJoinStyle = .round
-        backPath.addArc(withCenter: center, radius: radius, startAngle: 0, endAngle: CGFloat(Double.pi * 2), clockwise: true)
+        backPathColor.set()
         backPath.stroke()
 
-
-        let path = UIBezierPath()
-        path.lineWidth = lineWidth
+        let startAngle = CGFloat.pi * (-0.5)
+        let endAngle = CGFloat.pi * (-0.5) + CGFloat.pi * 2 * CGFloat(self.progress)
+        let path = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
         cycleColor?.set()
+        path.lineWidth = lineWidth
         path.lineCapStyle = .round
         path.lineJoinStyle = .round
-        let startAngle = CGFloat( Double.pi * (-0.5) + Double.pi * 2 * self.progress)
-        let endAngle = CGFloat(Double.pi * (-0.5))
-        path.addArc(withCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: false)
         path.stroke()
 
     }
-
+    // MARK: init func
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.addSubview(self.timerLabel)
+        let width = self.frame.size.width - lineWidth * 2 - Margin * 2
+        let height = self.frame.size.height - lineWidth * 2 - Margin * 2
+        self.timerLabel.frame = CGRect(x: (self.frame.size.height - height)  * 0.5, y: (self.frame.size.width - width)  * 0.5, width: width, height: height)
+        timerLabel.textColor = timerColor ?? UIColor.white
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    // MARK: property
+    /// 圆圈的宽度
+    var lineWidth: CGFloat = 5.0
+    private var displayLink: CADisplayLink?
+    var backPathColor: UIColor = UIColor.clear
+    /// 圆圈颜色
+    var cycleColor: UIColor?
+    /// 倒计时时间label的文字颜色
+    var timerColor: UIColor?
+    /// 中间显示的倒计时时间
+    var timerLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        return label
+    }()
+    
+    /// 自定义半径
+    var radius: CGFloat = 0.0
+    /// 自定义圆的中心点
+    var cycleCenter: CGPoint = CGPoint.zero
+    
+    // 倒计时时间
+    var duration: Double = 0.0 {
+        didSet {
+            if duration < 0.0 { return }
+            // 先停掉link
+            invalidateLink()
+            // 在开启link
+            self.displayLink = CADisplayLink(target: self, selector: #selector(countDown))
+            self.displayLink?.add(to: RunLoop.current, forMode: .commonModes)
+            
+        }
+    }
+    // 当前进度
+    var progress: Double = 1.0 {
+        didSet {
+            if progress < 0 || progress > 1 {
+                return
+            }
+            // ceil(返回不小于x的最小整数值)
+            self.timerLabel.text = "\(ceil(self.progress * duration))"
+            self.setNeedsDisplay()
+        }
+    }
 
 }
+
--- CycleCountDown/ViewController.swift
+++ CycleCountDown/ViewController.swift
@@ -9,18 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    deinit {
+        // 页面销毁的时候记得 call invalidateLink func
+        self.drawView?.invalidateLink()
+    }
+    var drawView: MZDrawView?
     override func viewDidLoad() {
         super.viewDidLoad()
-        let drawView = MZDrawView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
-        drawView.backPathColor = UIColor.red
-        drawView.cycleColor = UIColor.cyan
-        drawView.duration = 10
-        self.view.addSubview(drawView)
+        drawView = MZDrawView(frame: CGRect(x: 100, y: 100, width: 100, height: 200))
+        drawView?.backPathColor = UIColor.red
+        drawView?.cycleColor = UIColor.cyan
+        drawView?.duration = 10
+        // 半径
+        drawView?.radius = 20
+        self.view.addSubview(drawView!)
+        // Do any additional setup after loading the view.
     }
-    
-
-
-
 }
 
